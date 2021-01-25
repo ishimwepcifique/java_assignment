f32596999453a7b0e582a03b77646bfa33eb91b8
ishimwepcifique <77894995+ishimwepcifique@users.noreply.github.com>
2021-01-25 13:43:56 +0200

    Create array_Exercise1.java

---
+++ array_Exercise1.java
@@ -0,0 +1,11 @@
+class array_Exercise1
+{
+public void main(String[] args)
+ {
+String[] nums = {“ichi”,”ni”,”san”,”yon”,”go”};
+for ( int i=0; i< nums.length; i++) 
+{
+System.out.println(a[i]);
+}
+}
+}
